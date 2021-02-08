380356a76c34da0dcc815f589da5b6c2bfdacf0b
Doug Schallmoser <dougschallmoser@gmail.com>
2021-02-08 11:06:40 -0800

    import store, cart, and error components

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Store from '../components/Store';
+import Cart from '../components/Cart';
+import Error from '../components/Error';
 
 function Routes() {
   return (
