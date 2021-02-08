c71908f895b83d8c46c37e6e100930ac75cef98f
Doug Schallmoser <dougschallmoser@gmail.com>
2021-02-08 11:55:32 -0800

    load product images, create seed data for products

---
+++ src/seed/index.js
@@ -0,0 +1,50 @@
+export const products = [
+  {
+    id: 1,
+    name: "Sky Vibrancy",
+    price: 44.00,
+    photo: "/images/1.jpg",
+  },
+  {
+    id: 2,
+    name: "Summer Flowers",
+    price: 49.00,
+    photo: "/images/2.jpg"
+  },
+  {
+    id: 3,
+    name: "Mt. Baker Twilight",
+    price: 56.00,
+    photo: "/images/3.jpg"
+  },
+  {
+    id: 4,
+    name: "Big Views, Big Sure",
+    price: 40.00,
+    photo: "/images/4.jpg"
+  },
+  {
+    id: 5,
+    name: "Crater Lake in Winter",
+    price: 25.00,
+    photo: "/images/5.jpg"
+  },
+  {
+    id: 6,
+    name: "California Sun Haze",
+    price: 52.00,
+    photo: "/images/6.jpg"
+  },
+  {
+    id: 7,
+    name: "Away From the World",
+    price: 75.00,
+    photo: "/images/7.jpg"
+  },
+  {
+    id: 8,
+    name: "Joshua Tree Stars",
+    price: 61.00,
+    photo: "/images/8.jpg"
+  }
+];
