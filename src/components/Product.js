b422c7a91bedf2b8b0cae12f0b925767ee4a515a
Doug Schallmoser <dougschallmoser@gmail.com>
2021-02-11 12:16:25 -0800

    build button styled component, render styles based on if item has been added to cart

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,17 +1,38 @@
 import React, { useContext } from 'react';
 import './Product.css';
 import { CartContext } from '../contexts/CartContext';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  padding: 8px 20px;
+  background: ${props => props.added ? "white" : "black"};
+  color: ${props => props.added ? "black" : "white"};
+  border: ${props => props.added ? "2px solid black" : "2px solid transparent"};
+  cursor: pointer;
+  &:focus {
+    outline: none;
+  }
+  &:hover {
+    background: ${props => props.added ? "black" : ""};
+    color: ${props => props.added ? "white" : ""};
+  }
+`
 
 function Product({ data }) {
 
+  const { cartItems } = useContext(CartContext);
   const { addToCart } = useContext(CartContext);
 
+  const added = cartItems.some(item => item.id === data.id)
+  
   return (
     <div>
       <img src={`${data.photo}`} alt={data.name} />
       <h3>{data.name}</h3>
       <h2>${data.price.toFixed(2)}</h2>
-      <button onClick={() => addToCart()}>Add to Cart</button>
+      <Button added={added} onClick={() => addToCart(data)}>
+        {added ? "ADD ANOTHER" : "ADD TO CART"}
+      </Button>
     </div>
   )
 }
