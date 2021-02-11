52b1d01eea7c1b3643db5afbb6e272d898f6765e
Doug Schallmoser <dougschallmoser@gmail.com>
2021-02-11 11:38:15 -0800

    import CartContext and display number of items in cart

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import { CartContext } from '../contexts/CartContext';
 
 function Navbar() {
+
+  const { cartItems } = useContext(CartContext);
+
   return (
     <nav>
       <ul>
         <li><Link to="/store">Store</Link></li>
-        <li><Link to="/cart">Cart</Link></li>
+        <li><Link to="/cart">Cart ({cartItems.length})</Link></li>
       </ul>
     </nav>
   )
