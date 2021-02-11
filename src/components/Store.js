2043036a351ef60388cb1bfae9c396cf88d7751e
Doug Schallmoser <dougschallmoser@gmail.com>
2021-02-11 12:16:58 -0800

    display number of products available

--- src/components/Store.js
+++ src/components/Store.js
@@ -10,6 +10,7 @@ function Store() {
 
   return (
     <Layout>
+      <div className="products-length">({products.length}) Products Available</div>
       <div className="products-grid">
         {products.map(product => <Product key={product.id} data={product} />)}
       </div>
