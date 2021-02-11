121fa38ef0793b2e3580dca536e74d8a29cd3b99
Doug Schallmoser <dougschallmoser@gmail.com>
2021-02-11 11:38:52 -0800

    import addToCart function from context, add button with onclick to add item to cart

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Product.css';
+import { CartContext } from '../contexts/CartContext';
 
 function Product({ data }) {
+
+  const { addToCart } = useContext(CartContext);
+
   return (
     <div>
       <img src={`${data.photo}`} alt={data.name} />
       <h3>{data.name}</h3>
-      <h2>{data.price.toFixed(2)}</h2>
+      <h2>${data.price.toFixed(2)}</h2>
+      <button onClick={() => addToCart()}>Add to Cart</button>
     </div>
   )
 }
