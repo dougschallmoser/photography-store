f10749f65a62fa1006387a5ea9a0184131aaf597
Doug Schallmoser <dougschallmoser@gmail.com>
2021-02-12 12:15:43 -0800

    build helper function to tally up item count and total cost, modify ADD_ITEM case to accont for quantities

--- src/contexts/CartReducer.js
+++ src/contexts/CartReducer.js
@@ -1,9 +1,27 @@
+function sumItems(items) {
+  const cartCount = items.reduce((tot, item) => tot + item.quantity, 0)
+  const totalCost = items.reduce((total, item) => total + item.price * item.quantity, 0).toFixed(2)
+  return {
+    cartCount,
+    totalCost
+  }
+}
+
 function CartReducer(state, action) {
   switch (action.type) {
     case 'ADD_ITEM':
+      const item = state.cartItems.find(item => item.id === action.payload.id)
+
+      if (!item) {
+        state.cartItems.push({...action.payload, quantity: 1})
+      } else {
+        item.quantity++
+      }
+
       return {
-        ...state, 
-        cartItems: [...state.cartItems, action.payload]
+        ...state,
+        ...sumItems(state.cartItems),
+        cartItems: [...state.cartItems]
       }
     default: 
       return state
