8e7ef7b7a561a514a376bfbd2482f742fd4bd5c1
Doug Schallmoser <dougschallmoser@gmail.com>
2021-02-12 13:11:21 -0800

    build component by filling in divs with data

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -2,8 +2,25 @@ import React from 'react';
 
 function CartItem({ item }) {
   return (
-    <div>
-      {item.name} {item.quantity}
+    <div className="cart-item-container">
+      <div>
+        <img
+          className="product-image-cart"
+          src={`${item.photo}`}
+          alt={item.name}
+        />
+      </div>
+      <div>
+        <h3>{item.name}</h3>
+        <p>Price ${item.price.toFixed(2)}</p>
+      </div>
+      <div>
+        <p>Qty: {item.quantity}</p>
+      </div>
+      <div>
+        <button className="btn-increase-qty">&#43;</button>
+        <button className="btn-decrease-qty">&#8722;</button>
+      </div>
     </div>
   )
 }
