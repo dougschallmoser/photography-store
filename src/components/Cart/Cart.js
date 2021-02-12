9b1be7dccddaa28c71f4386dd101357d84e71dc2
Doug Schallmoser <dougschallmoser@gmail.com>
2021-02-12 12:16:21 -0800

    import cartCount and totalCost from Context and add p tags to display

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,20 +2,22 @@ import React, { useContext } from 'react';
 import Layout from '../Layout';
 import './Cart.css';
 import CartItems from './CartItems';
-import CartTotal from './CartTotal';
 import { CartContext } from '../../contexts/CartContext';
 
 function Cart() {
 
-  const { cartItems } = useContext(CartContext);
+  const { cartCount, totalCost } = useContext(CartContext);
 
   return (
     <Layout>
       <h1>Cart</h1>
-        {cartItems.length > 0 ? 
+        {cartCount > 0 ? 
           <div className="cart-container">
             <CartItems />
-            <CartTotal />
+            <div className="cart-total">
+              <p>Total Items: {cartCount}</p>
+              <p>Total Cost: {totalCost}</p>
+            </div>
           </div>
           : <h3>Your shopping cart is empty.</h3>
         }
