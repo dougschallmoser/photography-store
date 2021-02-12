a250d65f55a66b260bb6442e284dabfae22a4a5c
Doug Schallmoser <dougschallmoser@gmail.com>
2021-02-12 11:35:32 -0800

    wrap return in Layout component

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Layout from './Layout';
 
 function Error() {
   return (
-    <div>Error</div>
+    <Layout>
+      <h1>That page does not exist!</h1>
+    </Layout>
   )
 }
 
