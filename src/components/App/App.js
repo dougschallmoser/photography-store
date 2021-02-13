5d835dcf063288a62655fc360a6613e42b9e284e
Doug Schallmoser <dougschallmoser@gmail.com>
2021-02-13 13:20:20 -0800

    add subdirectories for App and Navbar components/stylesheets

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../Layout';
+import './App.css';
+
+function App() {
+  return (
+    <Layout>
+      <h2>WELCOME TO THE</h2>
+      <h1><span>PHOTOGRAPHY STORE</span></h1>
+      <h3>All photos taken by Doug Schallmoser</h3>
+    </Layout>
+  )
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import { CartContext } from '../../contexts/CartContext';
+import styled from 'styled-components';
+
+const Span = styled.span`
+  color: ${props => props.empty ? "black" : "#26ad5e"};
+`
+
+function Navbar() {
+
+  const { cartCount } = useContext(CartContext);
+
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/">MAIN</Link></li>
+        <li><Link to="/store">STORE</Link></li>
+        <li>
+          <img src={process.env.PUBLIC_URL + '/images/shopping_cart.svg'} alt="cart icon"/>
+          <Link to="/cart">
+            CART(<Span empty={cartCount === 0}>{cartCount}</Span>)
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar;
