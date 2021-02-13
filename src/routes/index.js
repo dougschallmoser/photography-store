1f75dc222c9cd7d8dad6cec458a2384c53085e8d
Doug Schallmoser <dougschallmoser@gmail.com>
2021-02-13 13:20:30 -0800

    update import path

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Store from '../components/Store/Store';
 import Cart from '../components/Cart/Cart';
+import App from '../components/App/App';
 import Error from '../components/Error';
 import './Global.css';
 
@@ -9,7 +10,7 @@ function Routes() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Store} />
+        <Route exact path="/" component={App} />
         <Route exact path="/store" component={Store} />
         <Route path="/cart" component={Cart} />
         <Route path="*" component={Error} />
