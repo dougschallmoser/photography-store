1402584914ab614b30ee8b7f1ef389eacdcf8934
Doug Schallmoser <dougschallmoser@gmail.com>
2021-02-13 14:20:36 -0800

    modify price key to have range of prices as an object

--- src/seed/index.js
+++ src/seed/index.js
@@ -2,61 +2,131 @@ export const products = [
   {
     id: 1,
     name: "Sky Vibrancy",
-    price: 44.00,
+    price: {
+      '5x7': 30.00,
+      '8x10': 55.00,
+      '11x14': 80.00,
+      '16x20': 120.00,
+      '24x30': 180.00,
+      '30x40': 230.00
+    },
     photo: "/images/1.jpg",
   },
   {
     id: 2,
     name: "Summer Flowers",
-    price: 49.00,
+    price: {
+      '5x7': 25.00,
+      '8x10': 50.00,
+      '11x14': 75.00,
+      '16x20': 115.00,
+      '24x30': 175.00,
+      '30x40': 225.00
+    },
     photo: "/images/2.jpg"
   },
   {
     id: 3,
     name: "Mt. Baker Twilight",
-    price: 56.00,
+    price: {
+      '5x7': 35.00,
+      '8x10': 60.00,
+      '11x14': 85.00,
+      '16x20': 125.00,
+      '24x30': 185.00,
+      '30x40': 235.00
+    },
     photo: "/images/3.jpg"
   },
   {
     id: 4,
     name: "Big Views, Big Sure",
-    price: 40.00,
+    price: {
+      '5x7': 40.00,
+      '8x10': 65.00,
+      '11x14': 90.00,
+      '16x20': 130.00,
+      '24x30': 190.00,
+      '30x40': 240.00
+    },
     photo: "/images/4.jpg"
   },
   {
     id: 5,
     name: "Crater Lake in Winter",
-    price: 25.00,
+    price: {
+      '5x7': 25.00,
+      '8x10': 50.00,
+      '11x14': 75.00,
+      '16x20': 115.00,
+      '24x30': 175.00,
+      '30x40': 225.00
+    },
     photo: "/images/5.jpg"
   },
   {
     id: 6,
     name: "California Sun Haze",
-    price: 52.00,
+    price: {
+      '5x7': 20.00,
+      '8x10': 45.00,
+      '11x14': 70.00,
+      '16x20': 110.00,
+      '24x30': 170.00,
+      '30x40': 220.00
+    },
     photo: "/images/6.jpg"
   },
   {
     id: 7,
     name: "Away From the World",
-    price: 75.00,
+    price: {
+      '5x7': 45.00,
+      '8x10': 70.00,
+      '11x14': 95.00,
+      '16x20': 135.00,
+      '24x30': 195.00,
+      '30x40': 245.00
+    },
     photo: "/images/7.jpg"
   },
   {
     id: 8,
     name: "Joshua Tree Stars",
-    price: 61.00,
+    price: {
+      '5x7': 15.00,
+      '8x10': 40.00,
+      '11x14': 65.00,
+      '16x20': 105.00,
+      '24x30': 165.00,
+      '30x40': 215.00
+    },
     photo: "/images/8.jpg"
   },
   {
     id: 9,
     name: "Autumn River",
-    price: 42.00,
+    price: {
+      '5x7': 20.00,
+      '8x10': 45.00,
+      '11x14': 70.00,
+      '16x20': 110.00,
+      '24x30': 170.00,
+      '30x40': 220.00
+    },
     photo: "/images/9.jpg"
   },
   {
     id: 10,
     name: "Purple Clouds",
-    price: 80.00,
+    price: {
+      '5x7': 45.00,
+      '8x10': 70.00,
+      '11x14': 95.00,
+      '16x20': 135.00,
+      '24x30': 195.00,
+      '30x40': 245.00
+    },
     photo: "/images/10.jpg"
   }
 ];
