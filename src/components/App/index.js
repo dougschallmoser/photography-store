fda9ffc390d3440cf71592d9341011316e51ee54
Doug Schallmoser <dougschallmoser@gmail.com>
2021-02-14 18:18:59 -0800

    add onLoad attribute to splash image, create new loader class

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../Layout';
 import './App.css';
 
 function App() {
+
+  const [imgLoaded, setImgLoaded] = useState(false);
+
   return (
     <Layout>
       <h2>WELCOME TO MY PHOTOGRAPHY STORE</h2>
       <div id="splash-image">
-        <img src="/images/splash.jpg" alt="Mountain" />
+        {!imgLoaded && <div className="loader-splash" />}
+        <img 
+          style={ imgLoaded ? {} : { visibility: "hidden" }} 
+          src="/images/splash.jpg" alt="Mountain" 
+          onLoad={() => setImgLoaded(true)}
+        />
       </div>
     </Layout>
   )
