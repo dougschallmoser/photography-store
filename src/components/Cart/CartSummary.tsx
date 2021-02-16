9554e8c982e58e8ecb7ad119a78a75fa164c17f8
Doug Schallmoser <dougschallmoser@gmail.com>
2021-02-16 09:38:12 -0800

    conditionally render StripeContainer component when checkout button is clicked

--- src/components/Cart/CartSummary.tsx
+++ src/components/Cart/CartSummary.tsx
@@ -1,10 +1,12 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { CartContext } from '../../contexts/CartContext';
+import StripeContainer from '../Stripe';
 import './Cart.css';
 
 function CartSummary() {
 
   const { cartCount, cartCost, clearCart } = useContext(CartContext);
+  const [openCheckout, setOpenCheckout] = useState(false);
 
   return (
     <div className="cart-total">
@@ -12,7 +14,12 @@ function CartSummary() {
       <h2>{cartCount}</h2>
       <p>Total Cost:</p>
       <h2>${cartCost}</h2>
-      <button className="checkout-btn">CHECKOUT</button>
+      <button onClick={() => setOpenCheckout(!openCheckout)} className="checkout-btn">CHECKOUT</button>
+      {openCheckout && 
+        <div className="checkout-modal">
+          <StripeContainer />
+        </div>
+      }
       <div onClick={() => clearCart()} id="clear-cart">CLEAR CART</div>
     </div>
   )
