9e06b56ef79052338d09f2da0d11802378d2cfbd
Doug Schallmoser <dougschallmoser@gmail.com>
2021-02-22 13:45:11 -0800

    change env variable to production stripe

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const stripe = require('stripe')(process.env.STRIPE_SECRET_TEST);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_PRODUCTION);
 const parser = require('body-parser');
 const cors = require('cors');
 
