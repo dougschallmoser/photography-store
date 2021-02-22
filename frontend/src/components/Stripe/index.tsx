d634d1b4bb781769596ab64616d17d6f418fc1ef
Doug Schallmoser <dougschallmoser@gmail.com>
2021-02-22 14:22:32 -0800

    add public key to constants, import into Stripe component

--- frontend/src/components/Stripe/index.tsx
+++ frontend/src/components/Stripe/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import CheckoutForm from './CheckoutForm';
+import { PUBLIC_KEY } from '../../constants';
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
-import CheckoutForm from './CheckoutForm';
 import './Stripe.css';
 
-const PUBLIC_KEY = "pk_live_51ILWQ0IPKzRqC9xealbBlIp3ZWZxmoejGljAww5G0RoNjTmUkXEI8MimVRbudOpenq8Lmtjg0OqII7fQ0A0taohq00USujbS9x"
 const stripeTestPromise = loadStripe(PUBLIC_KEY);
 
 function StripeContainer() {
--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,2 +1,3 @@
 export const API_ROOT = 'https://doug-photography-api.herokuapp.com';
 // export const API_ROOT = 'http://localhost:8080';
+export const PUBLIC_KEY = "pk_live_51ILWQ0IPKzRqC9xealbBlIp3ZWZxmoejGljAww5G0RoNjTmUkXEI8MimVRbudOpenq8Lmtjg0OqII7fQ0A0taohq00USujbS9x"
