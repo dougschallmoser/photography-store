472ec66d2b57956d9ae0ec91020a801a15d9e560
Doug Schallmoser <dougschallmoser@gmail.com>
2021-02-22 11:53:58 -0800

    build try catch block for rendering successful or failed stripe payment

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,27 @@ app.use(parser.json());
 app.use(cors());
 
 app.post('/stripe/charge', cors(), async (req, res) => {
-  console.log(req.body);
+  let { amount, id } = req.body;
+
+  try {
+    const payment = await stripe.paymentIntents.create({
+      amount,
+      currency: "USD",
+      description: "Doug Schallmoser Photography",
+      payment_method: id,
+      confirm: true
+    })
+
+    res.json({
+      message: 'Successful payment',
+      success: true
+    })
+  } catch (err) {
+    res.json({
+      message: 'Failed payment',
+      success: false
+    })
+  }
 })
 
 app.listen(process.env.PORT || 8080, () => {
